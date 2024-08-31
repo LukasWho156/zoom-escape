3ca46aa66e48f8321f1e1269719d568c763c317c
LukasWho156 <luschuel@googlemail.com>
2024-08-31 15:35:23 +0200

    move electron wrapper into dist folder

---
+++ dist/index.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron/main');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 528,
+    height: 624
+  })
+
+  win.setMenu(null);
+  win.resizable = false;
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
